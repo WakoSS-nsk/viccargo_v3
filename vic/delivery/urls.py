c868b29f7bcd315823a335795fa9d0134b4c7991
Vadim Kozlov <wadimkoslow11@gmail.com>
2022-09-17 13:15:19 +0300

    added styles, refactored the main page, footer and navigation

--- vic/delivery/urls.py
+++ vic/delivery/urls.py
@@ -6,4 +6,8 @@ app_name = 'delivery'
 
 urlpatterns = [
     path('', views.DeliveryView.as_view(), name='delivery'),
+    path('air/', views.AirDeliveryView.as_view(), name='air delivery'),
+    path('road/', views.RoadDeliveryView.as_view(), name='road delivery'),
+    path('train/', views.TrainDeliveryView.as_view(), name='train delivery'),
+    path('ship/', views.ShipDeliveryView.as_view(), name='ship delivery'),
 ]
--- vic/delivery/views.py
+++ vic/delivery/views.py
@@ -3,3 +3,19 @@ from django.views.generic.base import TemplateView
 
 class DeliveryView(TemplateView):
     template_name = 'delivery/delivery.html'
+
+
+class AirDeliveryView(TemplateView):
+    template_name = 'delivery/air.html'
+
+
+class RoadDeliveryView(TemplateView):
+    template_name = 'delivery/road.html'
+
+
+class TrainDeliveryView(TemplateView):
+    template_name = 'delivery/train.html'
+
+
+class ShipDeliveryView(TemplateView):
+    template_name = 'delivery/ship.html'
